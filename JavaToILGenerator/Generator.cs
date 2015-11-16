45169faab961d53a0cb7adcb145365048e56a3f7
Andrei Zholudzeu <Bozon22@gmail.com>
2015-11-16 21:53:30 +0300

    Added carcass of new generator

---
+++ JavaToILGenerator/Generator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace JavaToILGenerator
+{
+    public class Generator
+    {
+        public void GenerateExe()
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+            AssemblyName aName = new AssemblyName();
+            aName.Name = "CompiledJava";
+            AssemblyBuilder aBuilder = domain.DefineDynamicAssembly(aName, AssemblyBuilderAccess.Save);
+            ModuleBuilder mBuilder = aBuilder.DefineDynamicModule("JavaModule", "JavaCode.exe");
+
+        }
+    }
+}
